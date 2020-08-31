dfdbd586c4b503a7b346b6ddd9068c2fcf81cd08
Max Gabriel Susman <MGS@admins-Air.hsd1.ut.comcast.net>
2020-08-31 12:10:49 -0600

    first commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Reflection;
+
+namespace HelloWorld
+{
+    class TestAttribute : Attribute { }
+
+    [TestAttribute]
+    class MyTestSuite
+    {
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach(Type t in Assembly.GetExecutingAssembly().GetTypes())
+                Console.WriteLine(t.Name);
+        }
+    }
+}
